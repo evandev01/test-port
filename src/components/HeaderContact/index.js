60f7c155ed7106a30ac44614abf6f9164c151c8e
Bears <evanbero1980@gmail.com>
2021-01-19 09:23:42 -0600

    pushed minor changes to contact background

--- src/components/HeaderContact/index.js
+++ src/components/HeaderContact/index.js
@@ -3,12 +3,16 @@ import { Image, Container } from 'react-bootstrap';
 import './style.css';
 import 'animate.css';
 import headerImage from './assets/edev02.jpg';
+import reactImg from './assets/reactImg.png';
 
 function HeaderContact() {
     return (
         <div>
             <Container>
-                    <Image className='animate__animated animate__fadeInLeft' id='header' src={headerImage} />
+                    <Image className='animate__animated animate__fadeInLeft' id='header' src={headerImage}>
+                    {/* <Image src={reactImg}/> */}
+                    </Image>
+                    
             </Container>
         </div>
     )
